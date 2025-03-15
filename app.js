d19709fb1298cd53888a4c00432996d2081a96bc
Muhammad Tayyab <tayyabsultan621@gmail.com>
2025-03-15 13:29:46 +0500

    latest code

--- app.js
+++ app.js
@@ -15,9 +15,12 @@ app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
-app.use(
-  cors()
-);
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileupload());
 
