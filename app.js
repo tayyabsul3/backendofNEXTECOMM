abb0ef11c13765a3c656f05f75ed066f69e6754a
Muhammad Tayyab <tayyabsultan621@gmail.com>
2025-03-15 13:18:59 +0500

    Update app.js

--- app.js
+++ app.js
@@ -16,10 +16,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true, // Allow cookies to be sent with cross-origin requests
-  })
+  cors()
 );
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileupload());
