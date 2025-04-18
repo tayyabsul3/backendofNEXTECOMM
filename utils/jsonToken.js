409b573b9f1a1abc4256c59c92ac7135b8c985da
Muhammad Tayyab <tayyabsultan621@gmail.com>
2025-04-18 20:08:53 +0500

    Update jsonToken.js

--- utils/jsonToken.js
+++ utils/jsonToken.js
@@ -1,10 +1,27 @@
 const sendToken = async (user, statusCode, res) => {
   const token = await user.getJwtToken();
+const sendToken = async (user, statusCode, res) => {
+  const token = await user.getJwtToken();
+
+  const isProd = process.env.NODE_ENV === 'production';
 
   const options = {
-    expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-    httponly: true,
+    expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day
+    httpOnly: true,
+    secure: isProd, // must be true in production (HTTPS)
+    sameSite: "none", // fine for same-site
+    path: "/",
   };
+
+  res.status(statusCode).cookie("token", token, options).json({
+    sucess: true,
+    user,
+    token,
+  });
+};
+
+module.exports = sendToken;
+
 
   res.status(statusCode).cookie("token", token, options).json({
     sucess: true,
